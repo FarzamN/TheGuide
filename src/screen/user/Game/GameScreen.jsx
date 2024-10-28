7a291054abdbdf4b547d6b84488707a43b2470e7
FarzamN <frzamn64ml@gmail.com>
2024-10-28 21:09:12 +0500

    video player implamented

--- src/screen/user/Game/GameScreen.jsx
+++ src/screen/user/Game/GameScreen.jsx
@@ -3,6 +3,9 @@ import React, {useCallback, useEffect} from 'react';
 import {useFocusEffect} from '@react-navigation/native';
 import {GlobalStyle} from '../../../utils/GlobalStyle';
 import {Body, GameHeader} from '../../../components';
+import MoVideoPlayer from 'react-native-mo-video-player';
+import {width} from '../../../utils/Constants';
+import {style} from './style';
 
 const GameScreen = ({navigation}) => {
   useFocusEffect(
@@ -25,13 +28,25 @@ const GameScreen = ({navigation}) => {
 
   //   return () => backHandler.remove();
   // }, []);
+
   return (
     <Body>
       <GameHeader
         title={'New Testament'}
         subTitle={'angel gabriel visits zechariah'}
       />
-      <Text>GameScreen</Text>
+      <MoVideoPlayer
+        style={style.videoPlayer}
+        source={{
+          uri: 'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4',
+        }}
+        poster="https://pbs.twimg.com/media/FDX7UCbVcAUcNXj.jpg"
+        title="React Native MO-VIDEO-PLAYER"
+        autoPlay
+        playInBackground={false}
+        showHeader={false}
+        showSeeking10SecondsButton={false}
+      />
     </Body>
   );
 };
---
+++ src/screen/user/Game/style.js
@@ -0,0 +1,11 @@
+import {StyleSheet} from 'react-native';
+import {width} from '../../../utils/Constants';
+
+export const style = StyleSheet.create({
+  videoPlayer: {
+    width: width - 20,
+    height: 200,
+    borderRadius: 10,
+    overflow: 'hidden',
+  },
+});
