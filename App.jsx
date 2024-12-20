597ab32e543e86dacf67f6af34de691f5041a3c3
FarzamN <frzamn64ml@gmail.com>
2024-12-20 15:02:37 +0500

    appple

--- App.jsx
+++ App.jsx
@@ -14,6 +14,7 @@ const App = () => {
   const dispatch = useDispatch();
   const userDetails = useSelector(state => state.userDetails);
 
+  // hell
   useEffect(() => {
     fetchData();
   }, []);
