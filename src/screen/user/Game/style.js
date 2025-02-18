e1ced4b2968f869e1d8893a102d90048c9c8a82f
FarzamN <frzamn64ml@gmail.com>
2025-02-18 15:13:37 +0500

    game style updated

--- src/screen/user/Game/style.js
+++ src/screen/user/Game/style.js
@@ -1,12 +1,11 @@
 import {StyleSheet} from 'react-native';
 import {tab, width} from '../../../utils/Constants';
-import {Font} from '../../../utils/Font';
 import {Color} from '../../../utils/Color';
 
 export const style = StyleSheet.create({
   videoPlayer: {
-    width: width - 30,
-    height: tab ? 250 :  200,
+    width:tab ? width - 100 :  width - 30,
+    height:tab ? 250 : 200,
     borderRadius: 10,
     overflow: 'hidden',
     alignSelf: 'center',
--- src/screen/user/style.js
+++ src/screen/user/style.js
@@ -1,6 +1,6 @@
 import {StyleSheet} from 'react-native';
 import {Color} from '../../utils/Color';
-import { iOS, tab } from '../../utils/Constants';
+import {iOS, tab} from '../../utils/Constants';
 
 export const style = StyleSheet.create({
   listContainer: {
@@ -60,8 +60,8 @@ export const style = StyleSheet.create({
   // ------- Switch styles
   SwitchCont: {
     paddingHorizontal: tab ? 8 : 2,
-    paddingVertical: tab ? 8 : 2
-,    width: '90%',
+    paddingVertical: tab ? 8 : 2,
+    width: '90%',
     marginTop: 15,
     borderWidth: 1,
     borderRadius: 10,
@@ -90,7 +90,7 @@ export const style = StyleSheet.create({
     position: 'absolute',
     borderRadius: 100,
     right: 10,
-    top: iOS ? 80 : 10,
+    top: iOS ? 40 : 10,
     backgroundColor: 'black',
     borderRadius: 100,
     width: 30,
